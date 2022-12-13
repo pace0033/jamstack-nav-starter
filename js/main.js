535589ff0a64ba28ca21cae41544edeee3829b9e
Will Paceley <pace0033@algonquinlive.com>
2022-12-13 11:49:50 -0500

    dec 13th pa session

--- js/main.js
+++ js/main.js
@@ -5,6 +5,45 @@ const APP = {
     const userSearch = document.getElementById('userSearch');
     userSearch.addEventListener('submit', SEARCH.searchHandler);
     window.addEventListener('popstate', NAV.popstateHandler);
+
+    const splitArray = location.hash.split('/');
+
+    // Determine which page is loaded
+    if (document.body.id === 'index') {
+      // console.log("we're on index.html");
+      const [, type, query] = splitArray;
+
+      console.log(type, query);
+      console.log(decodeURIComponent(query));
+
+      API.fetchIndex(type, decodeURIComponent(query));
+    } else {
+      // THIS FIRES ON CREDITS.HTML
+      const [, type, id, title] = splitArray;
+      console.log(type, id, title);
+      API.fetchCredits(type, id);
+    }
+  },
+};
+
+const API = {
+  fetchIndex(type, query) {
+    console.log('We are fetching...');
+    console.log(`Finding results for media type ${type}`);
+    console.log(`Finding results for user input ${query}`);
+
+    // After data comes back from fetch
+    // in your .then()
+    // Build the DOM
+  },
+  fetchCredits(type, id) {
+    console.log('We are fetching...');
+    console.log(`Finding results for media type ${type}`);
+    console.log(`Finding results for credits id ${id}`);
+
+    // After data comes back from fetch
+    // in your .then()
+    // Build the DOM
   },
 };
 
@@ -22,6 +61,8 @@ const SEARCH = {
       NAV.updateURL(`/${mediaType}/${userInput}`);
     }
 
+    API.fetchIndex(mediaType, userInput);
+
     console.log(mediaType, userInput);
   },
 };
@@ -32,7 +73,23 @@ const NAV = {
   },
   popstateHandler: function (ev) {
     console.log('we are in popstate!');
-    console.log(window.location);
+    console.log(location);
+
+    const splitArray = location.hash.split('/');
+    if (document.body.id === 'index') {
+      // console.log("we're on index.html");
+      const [, type, query] = splitArray;
+
+      console.log(type, query);
+      console.log(decodeURIComponent(query));
+
+      API.fetchIndex(type, decodeURIComponent(query));
+    } else {
+      // THIS FIRES ON CREDITS.HTML
+      const [, type, id, title] = splitArray;
+      console.log(type, id, title);
+      API.fetchCredits(type, id);
+    }
   },
 };
 
